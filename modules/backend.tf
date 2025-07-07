74abd44bb0bde9e09351d1781cd89fb5acd305e1
rahul-deolikar <rahuldeolikar@outlook.com>
2025-07-07 23:09:33 +0530

    Delete modules directory
    replace data

--- modules/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket-name"
-    key            = "ecs-ec2-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-table"
-  }
-}
--- modules/ecs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.main.name
-}
-
-output "ecs_execution_role_arn" {
-  description = "ARN of the ECS execution role"
-  value       = aws_iam_role.ecs_execution_role.arn
-}
